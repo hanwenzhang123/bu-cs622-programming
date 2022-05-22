d91c094f8df51ac76773599408cb60474794e44e
Hanwen Zhang <hanwenzhang888@gmail.com>
2022-05-21 20:41:53 -0400

    Delete assignment1 directory

--- assignment1/src/map/Down.java
+++
@@ -1,9 +0,0 @@
-package map;
-
-public interface Down {
-    // Interface can not contain constructor
-    // all fields static and final
-    // all methods public and abstract
-    public abstract String goDown();
-    public abstract String goDown(int num);
-}
--- assignment1/src/map/Left.java
+++
@@ -1,9 +0,0 @@
-package map;
-
-public interface Left {
-    // Interface can not contain constructor
-    // all fields static and final
-    // all methods public and abstract
-    public abstract String goLeft();
-    public abstract String goLeft(int num);
-}
