c45acf8f530a32adfe545a97e52e62e359367e8a
hanwenzhang123 <hanwenzhang888@gmail.com>
2022-05-23 23:30:47 -0400

    io exception assignment 2 adding more comments

--- src/main/java/MergeContent.java
+++ src/main/java/MergeContent.java
@@ -5,14 +5,14 @@ import org.json.simple.parser.JSONParser;
 import java.io.*;
 
 public class MergeContent {
-    public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
+    public static void main(String[] args) {        //using external library org.json.simple to work with json data
+        JSONParser parser = new JSONParser();   //parser for read
         JSONArray array = new JSONArray();  //store all the map in arr for all files, which will be written in days.json
         JSONArray arr = new JSONArray();    //store map in arr for each file
 
         try{
             File file = new File("days.json");
-            if (!file.exists()) {
+            if (!file.exists()) {   //check if the file already exist or not, then get the absolute path the days.json file
                 file.createNewFile();
                 System.out.println("File already created. Located " + file.getAbsolutePath());
             } else {
@@ -32,32 +32,32 @@ public class MergeContent {
                     Object openDate = objectInfo.get("open_date");
                     Object closeDate = objectInfo.get("close_date");
 
-                    JSONObject obj = new JSONObject();
+                    JSONObject obj = new JSONObject();     //a new json object which will be used for each new key value pair and write into the new json file
                     obj.put("open_date", openDate);
                     obj.put("close_date", closeDate);
-                    arr.add(obj);
+                    arr.add(obj);   //add to arr for each json object scanned in one file
                 }
 
                 array.add(arr);     //each file we store in arr, then push to the total array
             }
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));   //create buffered writer, writing in stream to the new file days.json
             writer.write(array.toString()); //array will include all the dates for all the files
             writer.flush(); //flush into the file
         }
-        catch(FileNotFoundException e){
+        catch(FileNotFoundException e){  //catch the file not found exception
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        catch(IOException e){
+        catch(IOException e){   //catch the io exception
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        catch (Exception e) {   //general exception
+        catch (Exception e) {   //general exception if runtime error not belongs to above specific exception
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        finally {
+        finally {       //finally will always run no matter what
             System.out.println("Finally!!! Merge the content of all days into a single file done!!!");
         }
     }
--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFile {
-    public static void mergeContentTesting(String arg) {
+    public static void mergeContentTesting(String arg) {    //using external library org.json.simple to work with json data
         try{
             JSONParser parser = new JSONParser();
             FileReader reader = new FileReader(arg);
@@ -14,22 +14,22 @@ public class ReadFile {
 
             for (int i = 0; i < jsonArray.toArray().length; i++) {
                 Object eachObject = jsonArray.get(i);
-                System.out.println(eachObject);
+                System.out.println(eachObject);     //print out each json object data
             }
 
-        } catch(FileNotFoundException e){
+        } catch(FileNotFoundException e){  //catch the file not found exception
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        catch(IOException e){
+        catch(IOException e){   //catch the io exception
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        catch (Exception e) {
+        catch (Exception e) {     //general exception if runtime error not belongs to above specific exception
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        finally {
+        finally {     //finally will always run no matter what
             System.out.println("Finally!!! Just for testing output!!!");
         }
 
